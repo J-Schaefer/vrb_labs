f571f9e8dded2bd63526fa4bc781441c8156af8c
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2025-09-25 15:50:45 +0200

    Add initial setup of new dual-arm demo

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -6,6 +6,7 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
 # Add other startup programs here
+roslaunch giskardpy_ros giskardpy_tracy_standalone.launch
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
