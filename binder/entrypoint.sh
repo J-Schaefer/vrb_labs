1e3dcd33c719e8ef00df4aa62a84156d001c9fb8
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2025-09-30 17:39:33 +0200

    Fix binder install
    Add rviz config, remove XPRA desktop from webapps,
    fix requirements.txt.

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Launch the ROS core and web tools when containter starts
+# Launch the ROS core and web tools when container starts
 source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait giskardpy_ros giskardpy_tracy_standalone.launch &
-roslaunch --wait rvizweb rvizweb.launch &
+roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/binder/standalone_tracy.rviz &
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
