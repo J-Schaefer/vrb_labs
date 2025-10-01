8662d21e0878c2b47fcc1e124eebaea4b1f9ec82
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2025-10-01 10:39:45 +0200

    Fix some things in Dockerfiles
    Fixed paths if library in Dockerfile. Added new path for entrypoint
    in docker-compose file.

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,7 @@
 source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait giskardpy_ros giskardpy_tracy_standalone.launch &
-roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/binder/standalone_tracy.rviz &
+roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/work/binder/standalone_tracy.rviz &
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 jupyter lab workspaces import binder/vis-with-terminal.jupyterlab-workspace
